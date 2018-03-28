c94e00b16fa8ba50e6d1a731b5d09f2c1c8775ff
slowfeel <slowfeel@163.com>
2018-03-28 10:35:24 +0800

    change init

--- script/init.lua
+++ script/init.lua
@@ -11,6 +11,7 @@ local function loadLuaFile(name)
     return require(name)
 end
 
+
 --初始化一个文件名索引表
 initGlobalTable("Lnk")
 --初始化一个定义表,用来放所有枚举
@@ -22,7 +23,10 @@ initGlobalTable("Cfg")
 --初始化一个工具表,用来放工具方法
 initGlobalTable("Utl")
 --初始化一个实例表,用来放所有的实例
-initglobaltable("Ins")
+initGlobalTable("Ins")
+
+--初始化全局函数
+loadLuaFile("global")
 
 setmetatable(G,{__newIndex=function(t,k,v)
                     assert(false,"不可设置全局变量,请检查代码")
